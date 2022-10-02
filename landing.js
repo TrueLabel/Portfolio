072f8e436f18533b729945d581df3d68342f50b8
Zane Fink <zanefink@Zanes-MacBook-Pro.local>
2022-10-02 11:48:33 -0400

    pushing layout changes

--- landing.js
+++ landing.js
@@ -1,18 +1,4 @@
-
-
-
-
-
-
 $(() => {
-
-// $('.areaAvoid').hover(.css('backgound-color', 'red'))
-
-const $youLose = () => {
-  $('#gameText').text('You did not stay in the path! Refresh the page to try again!').css('color', 'white').css('font-size', '40px')
-  $('.areaAvoid').css('background-color', '#82091F')
-}
-// this is updating the page when user hovers target areas
 
 $('.hamburger').hide()
 // this hides the hamburger div on page load
@@ -21,9 +7,6 @@ const $toggleLinks = () => {
   $('.hamburger').toggle();
 }
 // this is working / this is for the links in the top right
-
-$('.areaAvoid').mouseover($youLose)
-// the waits for mouse hover on target area
 
 
 $('#hamburgerButton').on('click', $toggleLinks);
